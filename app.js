937476c3ab7ae95cdc3ba8657b796e616e0a8349
JseopKim <jsub5646@gmail.com>
2023-06-13 22:36:38 +0900

    대략의 레이아웃 구성

--- app.js
+++ app.js
@@ -1,5 +1,66 @@
-let firstDiv = document.createElement('div');
-document.body.appendChild(firstDiv);
+function tagCreate(tType, props) {
+  let element = document.createElement(tType);
+  for (let i in props) {
+    element[i] = props[i];
+  }
+  return element;
+};
 
-firstDiv.innerHTML = "김지섭의 페이지";
+function styleCreate(target, styleOb) {
+  for (i in styleOb) {
+    target.style[i] = styleOb[i];
+  }
+}
 
+styleCreate(document.body, {
+  margin: 0,
+})
+
+let root = tagCreate('div', {id: 'root'})
+document.body.appendChild(root);
+styleCreate(root, {
+  backgroundColor: '#F5F3C1',
+  width: '100vw',
+  height: '100vh'
+})
+
+let header = tagCreate('div', {id: 'header'});
+root.appendChild(header);
+styleCreate(header, {
+  width: '50%',
+  height: '30%',
+  backgroundColor: '#27E1C1',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+})
+
+header.innerText = "여긴 헤더입니다."
+
+let main = tagCreate('div', {id: 'main'});
+root.appendChild(main);
+
+styleCreate(main, {
+  width: '50%',
+  height: '30%',
+  backgroundColor: '#0EA293',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+})
+
+main.innerText = "여긴 메인입니다."
+
+let footer = tagCreate('div', {id: 'footer'});
+root.appendChild(footer);
+
+styleCreate(footer, {
+  width: '50%',
+  height: '30%',
+  backgroundColor: '#2F0F5D',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+})
+
+footer.innerText = "여긴 푸터입니다."
